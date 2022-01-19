e08eed99062d1907d377ea99d61cb3ca5657d140
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-01-19 23:53:54 +1100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -452,7 +452,7 @@ export default function Home() {
               </Link>
             </Text>
           </Box>
-          Fiscally sponsored by Hack Club Bank, a project by The Hack Foundation
+          Fiscally sponsored by the Hack Foundation
           (d.b.a. Hack Club), a 501(c)(3) nonprofit (EIN: 81-2908499).
         </Container>
       </Box>
