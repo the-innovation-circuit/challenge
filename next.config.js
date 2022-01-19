554472354f278bf290867cd83257919d922c40f9
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-01-19 22:12:16 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,16 +10,6 @@ module.exports = withMDX({
   async redirects() {
     return [
       {
-        source: '/photos',
-        destination: '/photos', // Matched parameters can be used in the destination
-        permanent: true,
-      },
-      {
-        source: '/welcome',
-        destination: '/welcome', // Matched parameters can be used in the destination
-        permanent: true,
-      },
-      {
         source: '/inr',
         destination: 'http://posters.innovation-challenge.co/inr', // Matched parameters can be used in the destination
         permanent: true,
