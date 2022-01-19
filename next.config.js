dc6b4f40685b32389376acc4b26ac40677f48a49
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-01-19 22:12:05 +1100

    hard code inr

--- next.config.js
+++ next.config.js
@@ -10,11 +10,6 @@ module.exports = withMDX({
   async redirects() {
     return [
       {
-        source: '/:slug',
-        destination: 'http://posters.innovation-challenge.co/:slug', // Matched parameters can be used in the destination
-        permanent: true,
-      },
-      {
         source: '/photos',
         destination: '/photos', // Matched parameters can be used in the destination
         permanent: true,
@@ -22,6 +17,11 @@ module.exports = withMDX({
       {
         source: '/welcome',
         destination: '/welcome', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+      {
+        source: '/inr',
+        destination: 'http://posters.innovation-challenge.co/inr', // Matched parameters can be used in the destination
         permanent: true,
       }
     ]
